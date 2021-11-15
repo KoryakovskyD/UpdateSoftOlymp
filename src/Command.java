466f1ef8cd9d6cc1eb7734b2debd4282cf04be89
KoryakovskyD <d.koryakovsky@gmail.com>
2021-11-15 20:04:52 +0300

    Added file output to the screen

--- src/Command.java
+++ src/Command.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 
 public class Command extends javax.swing.JFrame{
     public Command() {
@@ -13,14 +12,21 @@ public class Command extends javax.swing.JFrame{
 
         JLabel labelPrib = new JLabel(" Введите команду: ");
         labelPrib.setFont(new Font("Calibri", Font.PLAIN, 18));
-        JTextArea jTextAreaBrib = new JTextArea("");
-        jTextAreaBrib.setFont(new Font("Calibri", Font.PLAIN, 22));
-        JScrollPane jScrollPane = new JScrollPane(jTextAreaBrib);
+        JTextArea jTextAreaPrib = new JTextArea("");
+        jTextAreaPrib.setFont(new Font("Calibri", Font.PLAIN, 22));
+        JScrollPane jScrollPane = new JScrollPane(jTextAreaPrib);
+        JLabel labelCat = new JLabel(" Вывести файл на экран: ");
+        labelCat.setFont(new Font("Calibri", Font.PLAIN, 18));
+        JTextArea jTextAreaCat = new JTextArea("");
+        jTextAreaCat.setFont(new Font("Calibri", Font.PLAIN, 22));
+        JScrollPane jScrollPaneCat = new JScrollPane(jTextAreaCat);
         JButton button1 = new JButton("Далее");
         JButton button2 = new JButton("Назад");
 
         container.add(labelPrib);
         container.add(jScrollPane);
+        container.add(labelCat);
+        container.add(jScrollPaneCat);
         container.add(button2);
         container.add(button1);
 
@@ -29,9 +35,27 @@ public class Command extends javax.swing.JFrame{
             String ip_list = ChangePrib.getIp_list();
 
             // Пробежка по каждой машине
-            for (String ip:ip_list.split(" ")) {
-                BashCommand.pushCommand("sshpass -p wizard ssh root@" + ip + " \"" + jTextAreaBrib.getText() + "\"");
+            if (! jTextAreaPrib.getText().equals("")) {
+                for (String ip : ip_list.split(" ")) {
+                    BashCommand.pushCommand("sshpass -p wizard ssh root@" + ip + " \"" + jTextAreaPrib.getText() + "\"");
+                }
             }
+
+            if (! jTextAreaCat.getText().equals("")) {
+                for (String ip : ip_list.split(" ")) {
+
+                    String name = "";
+                    for (String curName : jTextAreaCat.getText().split("/")) {
+                        name = curName;
+                    }
+
+                    BashCommand.pushCommand("sshpass -p wizard ssh root@" + ip + " \"cp " + jTextAreaCat.getText() + " /home/mfp\"");
+                    BashCommand.pushCommand("sudo mount " + ip + ":/home/mfp /mnt/cdrom; sudo cp /mnt/cdrom/" +
+                            name + " /tmp; sudo rm /mnt/cdrom/" + name + "; sudo umount -l /mnt/cdrom");
+                    BashCommand.pushCommand("pluma /tmp/" + name + "; sudo rm /tmp/" + name);
+                }
+            }
+
             System.exit(0);
         });
 
